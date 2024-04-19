3ed7701b68be3ee0e7a4bf8efad750c720dd9e53
ASHUTOSH VISHWAKARMA <ashutoshvishwakarma208@outlook.com>
2024-04-19 17:27:27 +0530

    Added Website and Header Components

---
+++ client/src/components/Website.jsx
@@ -0,0 +1,11 @@
+import React from 'react'
+import {Header} from './'
+const Website = () => {
+  return (
+    <div>
+      <Header/>
+    </div>
+)
+}
+
+export default Website
---
+++ client/src/components/index.js
@@ -0,0 +1,6 @@
+import Header from './Header';
+import Website from  './Website'
+export {
+    Header,
+    Website
+}
